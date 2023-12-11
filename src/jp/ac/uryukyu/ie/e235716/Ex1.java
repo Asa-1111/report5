b739a0c732a63357ccc0805071968cd86b6c3cee
Asahi Nkasone <e235716@ie.u-ryukyu.ac.jp>
2023-12-12 02:05:59 +0900

    step1

---
+++ src/jp/ac/uryukyu/ie/e235716/Ex1.java
@@ -0,0 +1,12 @@
+package jp.ac.uryukyu.ie.e235716;
+
+public class Ex1{
+    public static void main(String[] args) {
+        String str = null;
+
+        
+        int length = str.length();
+        System.out.println("String length: " + length);
+         
+    }
+}
