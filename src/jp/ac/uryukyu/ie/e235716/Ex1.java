482d7b3e2f1c2377f5153f2231ba4d73cbc86925
Asahi Nkasone <e235716@ie.u-ryukyu.ac.jp>
2023-12-12 02:28:05 +0900

    step2

--- src/jp/ac/uryukyu/ie/e235716/Ex1.java
+++ src/jp/ac/uryukyu/ie/e235716/Ex1.java
@@ -4,9 +4,11 @@ public class Ex1{
     public static void main(String[] args) {
         String str = null;
 
-        
-        int length = str.length();
-        System.out.println("String length: " + length);
-         
+        try {
+            int length = str.length();
+            System.out.println("String length: " + length);
+        } catch(NullPointerException e) {
+            System.out.println("NullPointerException caught: " + e.getMessage());
+        }  
     }
 }
